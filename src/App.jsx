f3b36c688905c2b19c6834b68e30f9e17c36579d
Vilizar Denichin <denichinv@gmail.com>
2025-01-21 16:27:09 +0000

    font and basic structure of the app added

--- src/App.jsx
+++ src/App.jsx
@@ -4,10 +4,15 @@ import './App.css'
 function App() {
 
   return (
-    <>
-     <nav>This is nav</nav>
-     <main>this is main </main>
-    </>
+    <div>
+    <nav className="navbar">
+      <div className="nav-content">Navbar Content</div>
+    </nav>
+    <main className="main-content">
+      <h1>Movie Review App</h1>
+      <p>This is the main content area for our movie review application.</p>
+    </main>
+  </div>
   )
 }
 
