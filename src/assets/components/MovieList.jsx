ceed25cc23f762074d0a1eafe3ca378bfff16fcb
Vilizar Denichin <denichinv@gmail.com>
2025-01-24 16:41:26 +0000

    filter function implemented

--- src/assets/components/MovieList.jsx
+++ src/assets/components/MovieList.jsx
@@ -4,6 +4,8 @@ import MovieCard from './MovieCard'
 
 const MovieList = () => {
     const [movies, setMovies] = useState([])
+    const [allMoviesFiltered, setAllMoviesFiltered] = useState([])
+    const [givingRating,setRating] = useState(0)
     
     useEffect(() => {
         fetchmovies()
@@ -14,10 +16,23 @@ const MovieList = () => {
 
         const res = await fetch(`https://api.themoviedb.org/3/movie/popular?api_key=${apiKey}`);
         const data = await res.json();
-        console.log(data.results);
         setMovies(data.results);
+        setAllMoviesFiltered(data.results);
     }
     
+    const handleFilter = (rating) => {
+
+if (givingRating == rating) {
+
+    setRating(0)
+    setMovies(allMoviesFiltered)
+}else{
+    setRating(rating)
+    const filteredMovies = allMoviesFiltered.filter((movie) => movie.vote_average >= rating)
+    setMovies(filteredMovies)
+}
+
+    }
   
     return (
         <section className='movie_list'>
@@ -25,11 +40,11 @@ const MovieList = () => {
                 <h2 className='center_el movieh2head'>Popular</h2>
                 <div className='center_el movie_listadd'>
                     <ul className="center_el movie_filter">
-                        <li className="movie_filter_item">8+</li>
-                        <li className="movie_filter_item">7+</li>
-                        <li className="movie_filter_item">6+</li>
+                        <li className={givingRating === 6 ? "movie_filter_item active" : "movie_filter_item"} onClick={() => handleFilter(6)}>6+</li>
+                        <li className={givingRating === 7 ? "movie_filter_item active" : "movie_filter_item"} onClick={() => handleFilter(7)}>7+</li>
+                        <li className={givingRating === 8 ? "movie_filter_item active" : "movie_filter_item"} onClick={() => handleFilter(8)}>8+</li>
                     </ul>
-                    <select name="" id="" className="movie_sorting">
+                    <select name="" id="" className="movie_sorting"> 
                         <option value="">Sort By</option>
                         <option value="">Date</option>
                         <option value="">Rating</option>
@@ -41,9 +56,9 @@ const MovieList = () => {
                 </div>
             </header>
             <div className='movie_shows'>
-                {movies.map(movie => (
+                {movies.length > 0 ? (movies.map(movie => (
                     <MovieCard key={movie.id} movie={movie}/>
-                ))}
+                ))):(<p className='noMovies'>No movies found above this rating!</p>) }
             </div>
         </section>
     )
